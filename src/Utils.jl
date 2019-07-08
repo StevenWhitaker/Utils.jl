c887e7b5909f81e0015485eb42df0e513c8e4ec3
StevenWhitaker <steventwhitaker@gmail.com>
2019-07-08 17:10:46 -0400

    Added simple gradient descent and gradient projection method implementations, as well as simple (nonnegative) least squares solvers.

--- src/Utils.jl
+++ src/Utils.jl
@@ -2,12 +2,15 @@ module Utils
 
 using Plots
 using ImageSegmentation
+using LinearAlgebra
 
 include("plots.jl")
 include("arrays.jl")
+include("optimization.jl")
 
 export myplot, myplot!, myscatter, myscatter!, myhist, iplot, getroi, plotroi,
        segment
 export myfill, embed, @squeeze
+export gd, gpm, lsgd, nnlsgd
 
 end
