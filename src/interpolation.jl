cd25e898bb863d94cba43b11d48b7485de0c265b
StevenWhitaker <steventwhitaker@gmail.com>
2019-07-13 11:02:05 -0400

    Changed pos to spacing.

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -44,24 +44,25 @@ UnitSpacing(first::T, last::T) where {T} = UnitSpacing{T}(first, last)
 UnitSpacing(first, last) = UnitSpacing(promote(first, last)...)
 # TODO: Add constructor and conversion from UnitRange
 
-Base.size(pos::UnitSpacing) = (Int(pos.last - pos.first) + 1,)
-Base.getindex(pos::UnitSpacing, i::Int) = begin
+Base.size(spacing::UnitSpacing) = (Int(spacing.last - spacing.first) + 1,)
+Base.getindex(spacing::UnitSpacing, i::Int) = begin
     # It might be useful to be able to access outside of the array for extrapolation
-    # 1 <= i <= length(pos) || throw(BoundsError(pos, i))
-    pos.first + i - 1
+    # 1 <= i <= length(spacing) || throw(BoundsError(spacing, i))
+    spacing.first + i - 1
 end
-Base.getindex(pos::UnitSpacing, I::AbstractArray{Int,1}) =
-    [getindex(pos, i) for i in I]
+Base.getindex(spacing::UnitSpacing, I::AbstractArray{Int,1}) =
+    [getindex(spacing, i) for i in I]
 Base.IndexStyle(::Type{<:UnitSpacing}) = IndexLinear()
-Base.show(io::IO, pos::UnitSpacing) = print(io, pos.first, ":", pos.last)
-Base.show(io::IO, ::MIME"text/plain", pos::UnitSpacing{T}) where {T} =
-    print(io, "UnitSpacing{$T}: ", pos)
+Base.show(io::IO, spacing::UnitSpacing) =
+    print(io, spacing.first, ":", spacing.last)
+Base.show(io::IO, ::MIME"text/plain", spacing::UnitSpacing{T}) where {T} =
+    print(io, "UnitSpacing{$T}: ", spacing)
 
 # Not sure adding them makes sense, but I'll leave the code here just in case
-# Base.:+(pos1::UnitSpacing, pos2::UnitSpacing) = begin
-#     length(pos1) == length(pos2) ||
+# Base.:+(spacing1::UnitSpacing, spacing2::UnitSpacing) = begin
+#     length(spacing1) == length(spacing2) ||
 #         throw(ArgumentError("lengths of inputs must be equal"))
-#     ConstantSpacing(pos1.first + pos2.first, pos1.last + pos2.last, 2)
+#     ConstantSpacing(spacing1.first + spacing2.first, spacing1.last + spacing2.last, 2)
 # end
 
 """
@@ -93,7 +94,7 @@ ConstantSpacing(first::T, last::T, step::T) where {T} =
     ConstantSpacing{T}(first, last, step)
 ConstantSpacing(first::Float64, last::Float64, step::Float64) = begin
     # Without Base.TwicePrecision one experiences round-off errors that make
-    # pos[end] != pos.last
+    # spacing[end] != spacing.last
     # Still, though, 3 * 0.1 != 0.3 == prevfloat(3 * 0.1), even with TwicePrecision...
     T = Base.TwicePrecision{Float64}
     ConstantSpacing(T(first), T(last), T(step))
@@ -106,51 +107,53 @@ ConstantSpacing(first, last; nsteps::Integer) =
     ConstantSpacing(first, last, (last - first) / nsteps)
 # TODO: Add contructor and conversion from LinRange and Range (AbstractRange) objects
 
-Base.size(pos::ConstantSpacing) = (Int((pos.last - pos.first) / pos.step) + 1,)
-Base.getindex(pos::ConstantSpacing, i::Int) = begin
-    # 1 <= i <= length(pos) || throw(BoundsError(pos, i))
-    pos.first + (i - 1) * pos.step
+Base.size(spacing::ConstantSpacing) =
+    (Int((spacing.last - spacing.first) / spacing.step) + 1,)
+Base.getindex(spacing::ConstantSpacing, i::Int) = begin
+    # 1 <= i <= length(spacing) || throw(BoundsError(spacing, i))
+    spacing.first + (i - 1) * spacing.step
 end
-Base.getindex(pos::ConstantSpacing{Base.TwicePrecision{Float64}}, i::Int) =
-    Float64(pos.first + (i - 1) * pos.step)
-Base.getindex(pos::ConstantSpacing, I::AbstractArray{Int,1}) =
-    [getindex(pos, i) for i in I]
+Base.getindex(spacing::ConstantSpacing{Base.TwicePrecision{Float64}}, i::Int) =
+    Float64(spacing.first + (i - 1) * spacing.step)
+Base.getindex(spacing::ConstantSpacing, I::AbstractArray{Int,1}) =
+    [getindex(spacing, i) for i in I]
 Base.IndexStyle(::Type{<:ConstantSpacing}) = IndexLinear()
 Base.eltype(::ConstantSpacing{Base.TwicePrecision{Float64}}) = Float64
-Base.show(io::IO, pos::ConstantSpacing) =
-    print(io, pos.first, ":", pos.step, ":", pos.last)
-Base.show(io::IO, pos::ConstantSpacing{Base.TwicePrecision{Float64}}) =
-    print(io::IO, Float64(pos.first), ":", Float64(pos.step), ":", Float64(pos.last))
-Base.show(io::IO, ::MIME"text/plain", pos::ConstantSpacing{T}) where {T} =
-    print(io, "ConstantSpacing{$T}: ", pos)
-Base.show(io::IO, ::MIME"text/plain", pos::ConstantSpacing{Base.TwicePrecision{Float64}}) =
-    print(io, "ConstantSpacing{Float64}: ", pos)
+Base.show(io::IO, spacing::ConstantSpacing) =
+    print(io, spacing.first, ":", spacing.step, ":", spacing.last)
+Base.show(io::IO, spacing::ConstantSpacing{Base.TwicePrecision{Float64}}) =
+    print(io::IO, Float64(spacing.first), ":", Float64(spacing.step), ":", Float64(spacing.last))
+Base.show(io::IO, ::MIME"text/plain", spacing::ConstantSpacing{T}) where {T} =
+    print(io, "ConstantSpacing{$T}: ", spacing)
+Base.show(io::IO, ::MIME"text/plain", spacing::ConstantSpacing{Base.TwicePrecision{Float64}}) =
+    print(io, "ConstantSpacing{Float64}: ", spacing)
 
 """
-    VariableSpacing(pos) <: AbstractGridSpacing
+    VariableSpacing(spacing) <: AbstractGridSpacing
 
 Spacing between data points is not constant.
 
 # Properties
-- `pos::Array{<:Number,1}`: Sorted vector containing positions of data points
+- `spacing::Array{<:Number,1}`: Sorted vector containing positions of data points
 """
 struct VariableSpacing{T} <: AbstractGridSpacing{T}
-    pos::Array{T,1}
+    spacing::Array{T,1}
 
-    VariableSpacing{T}(pos::Array{T,1}) where {T} = begin
-        issorted(pos) || throw(ArgumentError("position vector must be sorted"))
-        new{T}(pos)
+    VariableSpacing{T}(spacing::Array{T,1}) where {T} = begin
+        issorted(spacing) ||
+            throw(ArgumentError("spacing vector must be sorted"))
+        new{T}(spacing)
     end
 end
-VariableSpacing(pos::Array{T,1}) where {T} = VariableSpacing{T}(pos)
-VariableSpacing(pos::AbstractArray{<:Any,1}) =
-    VariableSpacing(convert(Array, pos))
+VariableSpacing(spacing::Array{T,1}) where {T} = VariableSpacing{T}(spacing)
+VariableSpacing(spacing::AbstractArray{<:Any,1}) =
+    VariableSpacing(convert(Array, spacing))
 
-Base.size(pos::VariableSpacing) = size(pos.pos)
-Base.getindex(pos::VariableSpacing, i::Int) = pos.pos[i]
+Base.size(spacing::VariableSpacing) = size(spacing.spacing)
+Base.getindex(spacing::VariableSpacing, i::Int) = spacing.spacing[i]
 Base.IndexStyle(::Type{<:VariableSpacing}) = IndexLinear()
-Base.show(io::IO, pos::VariableSpacing{T}) where {T} =
-    print(io, "VariableSpacing{$T}", pos.pos)
+Base.show(io::IO, spacing::VariableSpacing{T}) where {T} =
+    print(io, "VariableSpacing{$T}", spacing.spacing)
 
 """
     AbstractNonGridSpacing <: AbstractSpacing
@@ -168,59 +171,48 @@ Abstract type for describing interpolators.
 """
 abstract type AbstractInterpolator{T,S<:SpacingTypes,N} end
 
+"""
+    NearestInterpolator(data[, spacing]) <: AbstractInterpolator
+
+Create a nearest-neighbor interpolator.
+
+# Properties
+- `data::Array`: Data used for interpolation
+- `spacing::Union{<:AbstractSpacing,<:Tuple{Vararg{<:AbstractGridSpacing}}} =
+    Tuple([UnitSpacing(1, size(data, n)) for n = 1:ndims(data)])`: Positions or
+    locations of the data points
+"""
 struct NearestInterpolator{T<:Number,S<:SpacingTypes,N} <: AbstractInterpolator{T,S,N}
     data::Array{T,N}
-    pos::S
+    spacing::S
 
-    NearestInterpolator{T,S,N}(data::Array{T,N}, pos::S) where {T<:Number,S<:SpacingTypes,N} = begin
-        # size(data) == ??? # TODO: Make sure sizes match
-        new{T,typeof(pos),N}(data, pos)
+    NearestInterpolator{T,S,N}(data::Array{T,N}, spacing::S) where {T<:Number,S<:SpacingTypes,N} = begin
+        (S <: AbstractSpacing ? size(data) == size(spacing) : all([size(data, n) == size(spacing[n], 1) for n = 1:N])) ||
+            throw(ArgumentError("size mismatch: each data point must have a position, and vice versa"))
+        new{T,typeof(spacing),N}(data, spacing)
     end
 end
-NearestInterpolator(data::Array{T,1}, pos::AbstractSpacing) where {T} =
-    NearestInterpolator{T,typeof(pos),1}(data, pos)
-NearestInterpolator(data::Array{T,N}, pos::Union{<:AbstractNonGridSpacing,<:Tuple{Vararg{<:AbstractGridSpacing,N}}}) where {T,N} =
-    NearestInterpolator{T,typeof(pos),N}(data, pos)
-NearestInterpolator(data::AbstractArray, pos::SpacingTypes) =
-    NearestInterpolator(convert(Array, data), pos)
+NearestInterpolator(data::Array{T,1}, spacing::S = UnitSpacing(1, length(data))) where {T,S<:AbstractSpacing} =
+    NearestInterpolator{T,S,1}(data, spacing)
+NearestInterpolator(data::Array{T,N}, spacing::Union{<:AbstractNonGridSpacing,<:Tuple{Vararg{<:AbstractGridSpacing,N}}} =
+    Tuple([UnitSpacing(1, size(data, n)) for n = 1:N])) where {T,N} =
+    NearestInterpolator{T,typeof(spacing),N}(data, spacing)
+NearestInterpolator(data::AbstractArray, spacing::SpacingTypes) =
+    NearestInterpolator(convert(Array, data), spacing)
+NearestInterpolator(data::AbstractArray) =
+    NearestInterpolator(convert(Array, data))
 
-# """
-#     NearestInterpolator([pos, ]val) <: AbstractInterpolator
-#
-# Create a nearest-neighbor interpolator.
-#
-# # Properties
-# - `pos::AbstractArray{<:AbstractArray{<:Any,1},1} = [1:N[i] for i = 1:D]`:
-#     Positions of corresponding values, i.e., the position of `val[i...]` is
-#     given by `(pos[1][i[1]], pos[2][i[2]], ...)`; the default is the
-#     traditional 1-based indexing, i.e., `pos[i...] == i`
-# - `val::Array`: Values from which to interpolate
-# """
-# struct NearestInterpolator{T,P<:AbstractArray{<:AbstractArray{<:Any,1},1},N} <: AbstractInterpolator
-#     val::Array{T,N}
-#     pos::P
-#
-#     NearestInterpolator(val::Array{T,N}, pos::P) where {T,P<:AbstractArray{<:AbstractArray{<:Any,1},1},N} = begin
-#         length(pos) == N ||
-#             throw(ArgumentError("length(pos) must be N"))
-#         size(val) == Tuple([length(pos[i]) for i = 1:N]) ||
-#             throw(ArgumentError("length of each element of pos must equal the size of the corresponding dimension of val"))
-#         new{T,P,N}(val, pos)
-#     end
-#
-#     NearestInterpolator(val::Array{T,N}) where {T,N} = begin
-#         pos = [1:size(val, n) for n = 1:N]
-#         new{T,typeof(pos),N}(val, pos)
-#     end
-# end
-#
-# """
-#     NearestInterpolator(pos)
-#
-# Find the nearest data point to the given position.
-# """
-# function (interp::NearestInterpolator{T,P,N})(pos::Vararg{Any,N}) where {T,P,N}
-#
-#     interp.val[[argmin(abs.(interp.pos[n] .- pos[n])) for n = 1:N]...]
-#
-# end
+Base.show(io::IO, interp::NearestInterpolator{T,S,N}) where {T,S,N} =
+    print(io, "NearestInterpolator{$T,$S,$N}:\n  data = ", interp.data, "\n  spacing = ", interp.spacing)
+
+"""
+    NearestInterpolator(pos)
+
+Find the nearest data point to the given position.
+"""
+function (interp::NearestInterpolator{T,S,N})(pos::Vararg{Any,N}) where {T,S,N}
+
+    index = findclosest(interp.spacing, pos)
+    return interp.data[index]
+
+end
